2c584a388220c2db9208a630c76c93de2b1166e9
Pololix <devpololix@gmail.com>
2026-05-03 17:44:08 +0200

    Added get_icon and get_icon_color

--- lua/easy-icons/init.lua
+++ lua/easy-icons/init.lua
@@ -2,10 +2,11 @@ local M = {}
 
 M.config = {}
 M.lookup = {
-    filetype = {},
-    filename = {},
-    dir      = {},
-    lsp      = {}
+    filestem  = {},
+    filename  = {},
+    extension = {},
+    -- dir      = {},
+    -- lsp      = {}
 }
 
 function M.setup(opts)
@@ -16,17 +17,42 @@ function M.load()
     -- sub web-devicons
     package.loaded["nvim-web-devicons"] = require("easy-icons")
 
-    -- build lookup table
-    for type, items in pairs(M.config) do
-        local table = type:match("^icon_(.+)")
-        if M.lookup[table] then
-            for entry, contents in pairs(items) do
-                M.lookup[table][entry] = contents
+    -- build lut
+    for kind, items in pairs(M.config) do
+        if M.lookup[kind] then
+            for entry, desc in pairs(items) do
+                M.lookup[kind][entry] = desc
             end
         else
-            -- warn
+            --warn
         end
     end
+end
+
+function M.has_loaded()
+    return true
+end
+
+function M.get_icon(name, ext, opts)
+    local stem = name:match("^(.*)%.")
+
+    if stem and M.lookup.filestem[stem] then
+        return M.lookup.filestem[stem].icon, M.lookup.filestem[stem].hl
+
+    elseif M.lookup.filename[name] then
+        return M.lookup.filename[name].icon, M.lookup.filename[name].hl
+
+    elseif ext and M.lookup.extension[ext] then
+        return M.lookup.extension[ext].icon, M.lookup.extension[ext].hl
+
+    else
+       return nil, nil
+   end
+end
+
+function M.get_icon_color(name, ext, opts)
+    local icon, hl = M.get_icon(name, ext, opts)
+    return icon, hl
 end
 
 return M
